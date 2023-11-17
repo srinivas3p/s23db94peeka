b0605adc4a860e81a662bdf4350ce647022cd43c
srinivas3p <87357998+srinivas3p@users.noreply.github.com>
2023-11-17 04:06:23 -0600

    hjbjh

--- app.js
+++ app.js
@@ -3,6 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -28,14 +48,28 @@ app.use('/users', usersRouter);
 app.use('/nwclass', nwclassRouter);
 app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
-app.use('/resource',resource);
+app.use('/resource', resource);
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -49,7 +83,7 @@ module.exports = app;
 
 require('dotenv').config();
 const connectionString =
-process.env.MONGO_CON
+  process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect(connectionString);
 
@@ -57,38 +91,39 @@ mongoose.connect(connectionString);
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
 
 // We can seed the collection if needed on
 //server start
-async function recreateDB(){
-// Delete everything
-await nwclass.deleteMany();
+async function recreateDB() {
+  // Delete everything
+  await nwclass.deleteMany();
 
-//first object
-let instance1 = new nwclass({ classification: 'Class ICBM', range: 500, name: 'W88', damage: 100 });
-instance1.save().then(doc=>{console.log("First object saved")}
-).catch(err=>{
-console.error(err)
-});
+  //first object
+  let instance1 = new nwclass({ classification: 'Class ICBM', range: 500, name: 'W88', damage: 100 });
+  instance1.save().then(doc => { console.log("First object saved") }
+  ).catch(err => {
+    console.error(err)
+  });
 
-//Second object
-let instance2 = new nwclass({ classification: 'Class Tactical', range: 750, name: 'B61', damage: 150 });
-instance2.save().then(doc=>{console.log("Second object saved")}
-).catch(err=>{
-console.error(err)
-});
+  //Second object
+  let instance2 = new nwclass({ classification: 'Class Tactical', range: 750, name: 'B61', damage: 150 });
+  instance2.save().then(doc => { console.log("Second object saved") }
+  ).catch(err => {
+    console.error(err)
+  });
 
-//Third object
-let instance3 = new nwclass({ classification: 'Class SLBM', range: 1000, name: 'Trident II D5', damage: 200 });
-instance3.save().then(doc=>{console.log("Third object saved")}
-).catch(err=>{
-console.error(err)
-});
+  //Third object
+  let instance3 = new nwclass({ classification: 'Class SLBM', range: 1000, name: 'Trident II D5', damage: 200 });
+  instance3.save().then(doc => { console.log("Third object saved") }
+  ).catch(err => {
+    console.error(err)
+  });
 }
 let reseed = true;
-if (reseed) {recreateDB();}
+if (reseed) { recreateDB(); }
 
 
 
