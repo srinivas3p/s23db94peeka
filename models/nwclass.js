30f1ef7e7212d437e0758589dfcd4d6a012f2745
srinivas3p <87357998+srinivas3p@users.noreply.github.com>
2023-11-29 18:55:05 -0600

    deploy commit

--- models/nwclass.js
+++ models/nwclass.js
@@ -3,8 +3,14 @@ const nwclassSchema = mongoose.Schema({
     //{ classification: 'Class ICBM', range: 500, name: 'W88', damage: 100 }
     //{ classification: "Class ICBM", range: 500, name: "W88", damage: 100 }
     classification: String,
-    range: Number,
-    name: String,
+    range: {type:Number,
+    required:true,
+    min:2,
+    max:444444},
+    name: {type:String,
+    required:true,
+    minlength:2,
+    maxlength:444444},
     damage: Number
 })
 module.exports = mongoose.model("nwclass", nwclassSchema)
--- routes/nwclass.js
+++ routes/nwclass.js
@@ -9,16 +9,25 @@ var express = require('express');
 const nwclass_controlers= require('../controllers/nwclass');
 var router = express.Router();
 /* GET nwclasss */
+// A little function to check if we have an authorized user and continue on
+
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ res.redirect("/login");
+ }
 router.get('/', nwclass_controlers.nwclass_view_all_Page );
 /* GET detail nwclass page */
-router.get('/detail', nwclass_controlers.nwclass_view_one_Page);
+router.get('/detail',secured, nwclass_controlers.nwclass_view_one_Page);
 
-router.get('/create', nwclass_controlers.nwclass_create_Page);
+router.get('/create',secured, nwclass_controlers.nwclass_create_Page);
 
 /* GET create update page */
-router.get('/update', nwclass_controlers.nwclass_update_Page);
+router.get('/update',secured, nwclass_controlers.nwclass_update_Page);
 
 /* GET delete nwclass page */
-router.get('/delete', nwclass_controlers.nwclass_delete_Page);
+router.get('/delete',secured, nwclass_controlers.nwclass_delete_Page);
 
 module.exports = router;
